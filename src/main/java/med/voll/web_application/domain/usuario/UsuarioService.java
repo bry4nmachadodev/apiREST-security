9cfb04439a0cef2715989191fecdc1a8cf062e82
bry4nmachadodev <bryanmachado1703@icloud.com>
2026-01-07 13:27:27 -0300

    criando construtores de usuários

--- src/main/java/med/voll/web_application/domain/usuario/UsuarioService.java
+++ src/main/java/med/voll/web_application/domain/usuario/UsuarioService.java
@@ -1,5 +1,9 @@
 package med.voll.web_application.domain.usuario;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import med.voll.web_application.domain.medico.Medico;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -19,4 +23,9 @@ public class UsuarioService implements UserDetailsService {
         return usuarioRepository.findByEmailIgnoreCase(username)
                 .orElseThrow(() -> new UsernameNotFoundException("O usuário não foi encontrado!"));
     }
+
+    public void SalvarUsuario(String nome, String email, String senha){
+        usuarioRepository.save(new Usuario(nome, email, senha));
+    }
+
 }
